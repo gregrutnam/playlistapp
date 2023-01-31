6cb9e461acd58dbd2bf44d449fdbfe31f39073f3
keirastanley <keiraestanley@gmail.com>
2023-01-31 19:36:51 +0000

    Swapped localhost back to deployed site as redirecturi

--- my-app/src/functions/spotify.js
+++ my-app/src/functions/spotify.js
@@ -2,9 +2,9 @@ import SpotifyWebApi from "spotify-web-api-js";
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-// const redirectUri = "https://playlistapp-nine.vercel.app/callback"
+const redirectUri = "https://playlistapp-nine.vercel.app/callback"
 
-const redirectUri = "http://localhost:3000/callback"
+// const redirectUri = "http://localhost:3000/callback"
 
 const clientId = "593d7fe3d674485392669714b0241c96"
 
